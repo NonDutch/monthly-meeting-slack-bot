3e594d0eca13cefdd461ca035497a11a590dabdb
Guilherme Pontes <guilhermepontes@msn.com>
2018-10-02 23:28:07 +0200

    Fix indentation

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ const webhook = new IncomingWebhook(url)
 
 const dropMessage = () => {
   const date = moment().startOf('month').add(14, 'days')
-	const wednesday = 3
-	let message = null
+  const wednesday = 3
+  let message = null
 
-	const currentWeekday = date.isoWeekday()
-	const missingDays = ((wednesday - currentWeekday) + 7) % 7
-	const nextMeeting = date.add(missingDays, 'days')
+  const currentWeekday = date.isoWeekday()
+  const missingDays = ((wednesday - currentWeekday) + 7) % 7
+  const nextMeeting = date.add(missingDays, 'days')
 
-	if (moment().isSame(moment(nextMeeting).add(-1, 'days'), 'day')) {
-		message = 'Just a friendly reminder the *Monthly Meeting* is going to happen tomorrow! :sparkles:'
-	}
+  if (moment().isSame(moment(nextMeeting).add(-1, 'days'), 'day')) {
+    message = 'Just a friendly reminder the *Monthly Meeting* is going to happen tomorrow! :sparkles:'
+  }
 
-	if (moment().isSame(moment(nextMeeting), 'day')) {
-		message = 'Hi people, the *Monthly Meeting* is today, see you there! :deal_with_it_parrot:'
-	}
+  if (moment().isSame(moment(nextMeeting), 'day')) {
+    message = 'Hi people, the *Monthly Meeting* is today, see you there! :deal_with_it_parrot:'
+  }
 
-	if (message) {
-		webhook.send(message)
-	}
+  if (message) {
+    webhook.send(message)
+  }
 }
 
 console.log('> Starting cron')
